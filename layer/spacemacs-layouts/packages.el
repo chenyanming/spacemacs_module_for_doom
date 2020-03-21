ea5b1c09644af64ade0a4b0518161465c3a54db0
Damon Chan <damonchan@DamondeiMac.local>
2020-03-21 13:57:12 +0800

    Added escape for layouts-transient-state

--- layer/spacemacs-layouts/packages.el
+++ layer/spacemacs-layouts/packages.el
@@ -211,6 +211,7 @@
         ("w" spacemacs/workspaces-transient-state/body :exit t)
         ("q" (lambda() (interactive) (setq hydra--work-around-dedicated nil)) :exit t)
         ("C-g" (lambda() (interactive) (setq hydra--work-around-dedicated nil)) :exit t)
+        ("<escape>" (lambda() (interactive) (setq hydra--work-around-dedicated nil)) :exit t)
         ("x" spacemacs/layouts-ts-kill)
         ("X" spacemacs/layouts-ts-kill-other :exit t))
       (spacemacs/set-leader-keys "l" '(lambda() (interactive) (setq hydra--work-around-dedicated t) (spacemacs/layouts-transient-state/body)))
