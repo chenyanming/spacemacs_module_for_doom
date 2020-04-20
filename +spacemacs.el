936904754a66f3e785443ba8e6a372b4e002cefc
Damon Chan <elecming@gmail.com>
2020-04-20 10:21:15 +0800

    Tidy up the codes, add spacemacs-path

--- +spacemacs.el
+++ +spacemacs.el
@@ -1,8 +1,12 @@
-;;; ~/.doom.d/+spacemacs.el -*- lexical-binding: t; -*-
+;;; +spacemacs.el -*- lexical-binding: t; -*-
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                                       ; Setup & load spacmeacs core packages ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Setup & load spacmeacs core packages
+
+(defcustom spacemacs-path (expand-file-name "~/.doom.d/modules/spacemacs/")
+ "spacemacs module path"
+ :group 'sapcemacs
+ :type 'string)
+
 (setq dotspacemacs-editing-style 'vim)
 (setq dotspacemacs-emacs-command-key "SPC")
 (setq dotspacemacs-emacs-leader-key "M-m")
@@ -14,27 +18,26 @@
 (setq which-key-idle-delay 0.4)
 (setq which-key-idle-secondary-delay 0.1)
 
-;; Dot file location - SPC-f-e-d
 (defun dotspacemacs/location ()
+  "Dot file location - SPC-f-e-d"
   ;; (doom/find-file-in-private-config)
-  (doom/goto-private-config-file)
-  )
+  (doom/goto-private-config-file))
 
 ;; (setq dotspacemacs-persistent-server t)
-(load! "~/.doom.d/modules/spacemacs/core/core-funcs.el")
-; (load! "~/.doom.d/modules/spacemacs/core/core-spacemacs-buffer.el")
-;; (load! "~/.doom.d/modules/spacemacs/core/core-fonts-support.el")
-;; (load! "~/.doom.d/modules/spacemacs/core/core-dumper.el")
-(load! "~/.doom.d/modules/spacemacs/core/core-keybindings.el")
-(load! "~/.doom.d/modules/spacemacs/core/core-transient-state.el")
-(load! "~/.doom.d/modules/spacemacs/core/core-toggle.el")
-;; (load! "~/.doom.d/modules/spacemacs/core/core-hooks.el")
-;; (load! "~/.doom.d/modules/spacemacs/core/core-fonts-support.el")
+(load! (concat spacemacs-path "core/core-funcs.el"))
+; (load! (concat spacemacs-path "core/core-spacemacs-buffer.el")
+;; (load! (concat spacemacs-path "core/core-fonts-support.el")
+;; (load! (concat spacemacs-path "core/core-dumper.el")
+(load! (concat spacemacs-path "core/core-keybindings.el"))
+(load! (concat spacemacs-path "core/core-transient-state.el"))
+(load! (concat spacemacs-path "core/core-toggle.el"))
+;; (load! (concat spacemacs-path "core/core-hooks.el")
+;; (load! (concat spacemacs-path "core/core-fonts-support.el")
 ;; TODO: evilified-state-evilify-map seems to have conflict with the doom
 ;; setting, like the leader key setting.
 ;; (use-package! evil-evilified-state
 ;;   :load-path
-;;   "~/.doom.d/modules/spacemacs/evil-evilified-state")
+;;   (concat spacemacs-path "evil-evilified-state")
 
 ;; (require 'bind-map)
 ;; (require 'core-funcs)
@@ -58,11 +61,8 @@ defer call using `spacemacs-post-user-config-hook'."
 
 (setq version-control-diff-tool 'git-gutter)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                               ; redifine the spacemcas/set-leader-keys macro ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (defmacro spacemacs/set-leader-keys (&rest rest)
+  "redefine the spacemcas/set-leader-keys macro"
   ;; (setq doom--map-fn 'doom--define-leader-key)
   ;; (doom--map-keyword-to-states :n)
   ;; (setq doom--map-state '(:n t))
@@ -71,15 +71,6 @@ defer call using `spacemacs-post-user-config-hook'."
   ;; (doom--map-process (cons '(:n) 'rest))
   ;; (doom--map-process (apply 'concat :n  rest))
   )
-
-;; (cond (t 6 7))
-;; ( listp (cons :n 7) )
-
-;; (spacemacs/set-leader-keys
-;;   "s-;" 'vc-next-action)
-
-;; (spacemacs/set-leader-keys
-;; (map! :leader :n )
 
 (defvar dotspacemacs-default-layout-name "Default"
   "Name of the default layout.")
@@ -94,10 +85,7 @@ Useful for making the home buffer the only visible buffer in the frame."
   (delete-other-windows))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                                        ;  unmap the doom original key prefix ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;;; unmap the doom original key prefix
 (map! :leader
       "gg" nil
       "bN" nil
@@ -112,56 +100,36 @@ Useful for making the home buffer the only visible buffer in the frame."
       "x" nil)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                             ;  load the modified spacemacs layers packages   ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load! "~/.doom.d/modules/spacemacs/layer/org/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/pdf/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/epub/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/dash/packages.el")
-;; (load! "~/.doom.d/modules/spacemacs/layer/dash/config.el")
-;; (load! "~/.doom.d/modules/spacemacs/layer/dash/funcs.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-defaults/funcs.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-defaults/keybindings.el")
-;; (load! "~/.doom.d/modules/spacemacs/layer/version-control/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/version-control/keybindings.el")
-(load! "~/.doom.d/modules/spacemacs/layer/version-control/funcs.el")
-(load! "~/.doom.d/modules/spacemacs/layer/version-control/config.el")
-(load! "~/.doom.d/modules/spacemacs/layer/github/funcs.el")
-(load! "~/.doom.d/modules/spacemacs/layer/github/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-navigation/funcs.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-navigation/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-layouts/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-layouts/funcs.el")
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-layouts/config.el")
-(load! "~/.doom.d/modules/spacemacs/layer/bm/packages.el")
+;;;  load the modified spacemacs layers packages
+(load! (concat spacemacs-path "layer/org/packages.el"))
+(load! (concat spacemacs-path "layer/pdf/packages.el"))
+(load! (concat spacemacs-path "layer/epub/packages.el"))
+(load! (concat spacemacs-path "layer/dash/packages.el"))
+;; (load! (concat spacemacs-path "layer/dash/config.el"))
+;; (load! (concat spacemacs-path "layer/dash/funcs.el"))
+(load! (concat spacemacs-path "layer/spacemacs-defaults/funcs.el"))
+(load! (concat spacemacs-path "layer/spacemacs-defaults/keybindings.el"))
+;; (load! (concat spacemacs-path "layer/version-control/packages.el"))
+(load! (concat spacemacs-path "layer/version-control/keybindings.el"))
+(load! (concat spacemacs-path "layer/version-control/funcs.el"))
+(load! (concat spacemacs-path "layer/version-control/config.el"))
+(load! (concat spacemacs-path "layer/github/funcs.el"))
+(load! (concat spacemacs-path "layer/github/packages.el"))
+(load! (concat spacemacs-path "layer/spacemacs-navigation/funcs.el"))
+(load! (concat spacemacs-path "layer/spacemacs-navigation/packages.el"))
+(load! (concat spacemacs-path "layer/spacemacs-layouts/packages.el"))
+(load! (concat spacemacs-path "layer/spacemacs-layouts/funcs.el"))
+(load! (concat spacemacs-path "layer/spacemacs-layouts/config.el"))
+(load! (concat spacemacs-path "layer/bm/packages.el"))
 
-(load! "~/.doom.d/modules/spacemacs/layer/git/packages.el")
+(load! (concat spacemacs-path "layer/git/packages.el"))
 
-(load! "~/.doom.d/modules/spacemacs/layer/javascript/packages.el")
-(load! "~/.doom.d/modules/spacemacs/layer/javascript/config.el")
-(load! "~/.doom.d/modules/spacemacs/layer/javascript/funcs.el")
+(load! (concat spacemacs-path "layer/javascript/packages.el"))
+(load! (concat spacemacs-path "layer/javascript/config.el"))
+(load! (concat spacemacs-path "layer/javascript/funcs.el"))
+(load! (concat spacemacs-path "layer/spacemacs-editing/packages.el"))
 
-(load! "~/.doom.d/modules/spacemacs/layer/spacemacs-editing/packages.el")
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                               ; load the spacemacs original layers packages  ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (load! "~/.emacs.d.spacemacs/layers/+spacemacs/spacemacs-defaults/packages.el")
-;; (load! "~/.emacs.d.spacemacs/layers/+spacemacs/spacemacs-defaults/keybindings.el")
-;; (load! "~/.emacs.d.spacemacs/layers/+spacemacs/spacemacs-defaults/funcs.el")
-;; (load! "~/.emacs.d.spacemacs/layers/+readers/pdf/packages.el")
-
-
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                                        ;         initialise layers          ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; initialise layers
 
 ; (spacemacs-defaults/init-abbrev)
 ; (spacemacs-defaults/init-archive-mode)
@@ -236,7 +204,7 @@ Useful for making the home buffer the only visible buffer in the frame."
 ;;(dash/init-counsel-dash)
 
 
-(setq hydra--work-around-dedicated nil) ;; help lv to work https://github.com/abo-abo/hydra/issues/329, even setup this, lv can still has bug with SPC-l-n or SPC-l-p
+(setq hydra--work-around-dedicated nil) ; help lv to work https://github.com/abo-abo/hydra/issues/329, even setup this, lv can still has bug with SPC-l-n or SPC-l-p
 ;; "message" is flickering when SPC-w.-[or]
 ;;(setq hydra-hint-display-type 'message)
 ;; since "lv" is not work well on doom even set work around to nil, SPC-l-n or p will create relundant windows
