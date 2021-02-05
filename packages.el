1205c34affd8435df62cbe628424ba962ba48dea
Damon Chan <damon.chan@freshup.com.hk>
2021-02-05 12:48:35 +0800

    Rename +spacmeacs.el to config.el, add packages.el.
    To follow doom module's setting. So that we can add in init.el.

---
+++ packages.el
@@ -0,0 +1,16 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/spacemacs/packages.el
+
+(package! nov) ;; for epub layer
+(package! auto-highlight-symbol) ;; for spacemacs-navigation
+(package! eyebrowse) ;; for spacemacs-layouts
+(package! bm) ;; for bm layer
+(package! evil-iedit-state) ;; for spacemacs-navigation
+(package! bind-map)
+(package! symbol-overlay)
+(package! move-text) ;; for spacemacs-editing
+(package! string-inflection) ;; for spacemacs-editing
+(package! forge) ;; for github layer
+(package! evil-terminal-cursor-changer) ;; for spacemacs evil
+(package! eshell-prompt-extras) ;; for shell layer
+(package! pdf-tools) ;; for pdf layer
